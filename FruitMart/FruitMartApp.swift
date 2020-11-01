c62285576ba7bccace942b9c55fcfdcffcdde20a
조준형 <junhyung@edu.hanbat.ac.kr>
2020-11-01 16:43:08 +0900

    Initial Commit

---
+++ FruitMart/FruitMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMartApp.swift
+//  FruitMart
+//
+//  Created by 조준형 on 2020/11/01.
+//
+
+import SwiftUI
+
+@main
+struct FruitMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
