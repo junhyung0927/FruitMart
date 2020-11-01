f287534a0d7c056cbe9ddcc188989fead417a44f
조준형 <junhyung@edu.hanbat.ac.kr>
2020-11-01 19:36:17 +0900

    5.4 주문 모델 추가

---
+++ FruitMart/Source/Model/Order.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct Order: Identifiable {
+    //식별을 위한 Identifiable 프로토콜 채택
+    static var orderSequence = sequence(first: 1) { $0 + 1 }
+    
+    let id: Int
+    let product: Product
+    let quantitiy: Int
+    
+    var price: Int{
+        product.price * quantitiy
+    }
+}
--- FruitMart/Source/Model/Store.swift
+++ FruitMart/Source/Model/Store.swift
@@ -2,9 +2,17 @@ import Foundation
 
 final class Store : ObservableObject{
     @Published var products: [Product]
+    @Published var orders: [Order] = [] //전체 주문 목록
     
     init(filename: String = "ProductData.json") {
         self.products = Bundle.main.decode(filename: filename, as : [Product].self)
+    }
+    
+    func placeOrder(product: Product, quantity: Int){
+        let nextID = Order.orderSequence.next()!
+        let order = Order(id: nextID, product: product, quantitiy: quantity)
+        orders.append(order)
+        //어떤 상품을 받으면 placeOrder 메서드에 주문한 상품과 수량 정보를 받아서 orders 프로퍼티에 저장된다.
     }
 }
 extension Store{
