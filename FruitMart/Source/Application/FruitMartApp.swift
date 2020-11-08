3cfb300c83f9d79029c0e1e37dc940cd2515bd84
조준형 <whwnsgud22@naver.com>
2020-11-08 19:29:32 +0900

    6. 기본 UI 색상 변경하기 (IOS14)

--- FruitMart/Source/Application/FruitMartApp.swift
+++ FruitMart/Source/Application/FruitMartApp.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 @main
-struct FruitMartApp: App {
+struct FruitMartApp: App{
     var body: some Scene {
+        //WindowGroup은 SwiftUI View를 담고 있는 container scene 같은 것.
         WindowGroup {
             let rootView = Home()
                 .environmentObject(Store())
         }
     }
-
+    
+    //ios 14 SwiftUI
+    init(){
+        //large 디스플레이 모드에서 적용
+        UINavigationBar.appearance().largeTitleTextAttributes = [
+            .foregroundColor: UIColor(named: "peach")!
+        ]
+        
+        //inline 디스플레이 모드일 때 사용
+        UINavigationBar.appearance().titleTextAttributes = [
+            .foregroundColor: UIColor(named: "peach")!
+        ]
+    }
 }
