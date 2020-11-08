a4ddda833b4b8fe3ef47d05963af9ae54cfda959
조준형 <whwnsgud22@naver.com>
2020-11-08 20:04:47 +0900

    7. 스크롤 뷰 추가

--- FruitMart/Source/User Interface/Home/Home.swift
+++ FruitMart/Source/User Interface/Home/Home.swift
@@ -10,14 +10,17 @@ import SwiftUI
 struct Home: View {
     @EnvironmentObject private var store: Store
     @State private var quickOrder: Product? //빠른 주문 기능으로 주문한 상품 저장
+    @State private var showingFavoriteImage: Bool = true
     
     var body: some View {
         NavigationView{
-            List(store.products) { product in
-                NavigationLink(destination: ProductDetailView(product: product)){
-                    ProductRow(product: product, quickOrder: self.$quickOrder)
+            VStack{
+                //즐겨찾기 상품이 없다면 무시
+                if showFavorite {
+                    favoriteProducts //구현해 둔 스크롤 뷰에 해당하는 프로퍼티
                 }
-                .buttonStyle(PlainButtonStyle())
+                darkerDivider
+                productList // 기존에 있던 코드를 프로퍼티로 추출
             }
             .navigationBarTitle("과일마트")
         }
@@ -34,6 +37,36 @@ struct Home: View {
             
             OrderCompltedMessasge()
         }
+    }
+    
+    //즐겨찾기 상품 목록
+    var favoriteProducts: some View {
+        FavoriteProductScrollView(showingImage: $showingFavoriteImage)
+            .padding(.top, 24)
+            .padding(.bottom, 8)
+    }
+    
+    //커스텀 구분선
+    var darkerDivider: some View{
+        Color.primary
+            .opacity(0.3)
+            .frame(maxWidth: .infinity, maxHeight: 1)
+            
+    }
+    
+    //body에 작성되어 있던 기존 코드 추출
+    var productList: some View{
+        List(store.products) { product in
+            NavigationLink(destination: ProductDetailView(product: product)){
+                ProductRow(product: product, quickOrder: self.$quickOrder)
+            }
+            .buttonStyle(PlainButtonStyle())
+        }
+    }
+    
+    //즐겨찾기 상품 유무 확인
+    var showFavorite: Bool{
+        !store.products.filter({ $0.isFavorite}).isEmpty
     }
 }
 
